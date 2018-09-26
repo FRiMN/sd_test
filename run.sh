484f03ff910302a13b848eb614433d670cc000c1
NV <freezemandix@ya.ru>
2018-09-27 01:01:04 +0700

    add info in run script

--- run.sh
+++ run.sh
@@ -3,5 +3,7 @@
 venv_python=./venv/bin/python
 
 ${venv_python} ./manage.py migrate
+echo 'Start parse_server and detach...'
 (${venv_python} ./manage.py parse_server &) &
+echo 'Start runserver and detach...'
 (${venv_python} ./manage.py runserver 8000 &) &
